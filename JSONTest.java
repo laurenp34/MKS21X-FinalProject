1f3921dfd9f8a3ce5ef544516465f6cab697ef35
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-03 20:23:21 -0500

    basic file access, for purposes of testing lib

--- JSONTest.java
+++ JSONTest.java
@@ -1,8 +1,16 @@
 import com.google.gson.Gson;
+import java.io.*;
+import java.util.*;
+
+
 public class JSONTest {
   //simply testing whether this will commit/push
-  public static void main(String[] args){
-    Gson file = new Gson();
+  public static void main(String[] args) throws FileNotFoundException{
+    Gson obj = new Gson();
     System.out.println("library access worked!");
+    Scanner sca = new Scanner(new File("samplefile.json"));
+    String inp = "";
+    while(sca.hasNextLine()) inp += sca.nextLine()+"\n";
+    System.out.println(inp);
   }
 }
