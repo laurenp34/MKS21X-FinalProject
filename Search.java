dc5b219b8b6644d54c8d773cb5b245d5033fc12c
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-05 16:22:49 -0500

    restructured reader to be JsonReader from library

--- Search.java
+++ Search.java
@@ -3,6 +3,7 @@ import com.google.gson.Gson;
 import com.google.gson.FieldAttributes;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonArray;
+import com.google.gson.stream.JsonReader;
 import com.google.*;
 import java.io.*;
 import java.util.*;
@@ -16,13 +17,8 @@ public class Search{
     Search out = null;
     try{
       URL webpage = new URL("https://openlibrary.org/search.json?q="+searchTerm);
-      String json = "";
-      //code from oracle official tutorials, https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
-      BufferedReader in = new BufferedReader(new InputStreamReader(webpage.openStream()));
-      String inputLine;
-      while ((inputLine = in.readLine()) != null)
-          json += inputLine + "\n";
-      in.close();
+      Reader isr = new InputStreamReader(webpage.openStream());
+      JsonReader json = new JsonReader(isr);
       Gson g = new Gson();
       out = g.fromJson(json,Search.class);
       out.searchTerm = searchTerm;
