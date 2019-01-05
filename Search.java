ba395fa897776b2bbb729886a90dc8e4462cd1e8
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-05 16:36:51 -0500

    added comments for explanation

--- Search.java
+++ Search.java
@@ -17,7 +17,7 @@ public class Search{
       Reader json = new InputStreamReader(webpage.openStream());
       //for accessing nonstatic methods in Gson class
       Gson g = new Gson();
-      //parses data from json file into
+      //parses data from json file into an object
       out = g.fromJson(json,Search.class);
       out.searchTerm = searchTerm;
     }catch(MalformedURLException e){
@@ -28,7 +28,10 @@ public class Search{
     return out;
   }
   public static void main(String[] args){//for testing purposes
+    // method used here effectively converts simplistic search terms from args
+    // can be made more indepth and input can come from places other than shell args
     Search s = buildSearch(String.join("+",args));
+    //printing of various variables to demonstrate successful conversion
     System.out.println(s.numFound);
     for(int i=0;i<s.docs.length;i++){
       System.out.println(s.docs[i].title);
