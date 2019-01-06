62f6fdfdf59c0af3a03dc067b4633794272460c1
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-05 20:28:49 -0500

    new approach to search box: for every key press add to a string searchTerm, then when enter is pressed you end the program / end the search.

--- Search.java
+++ Search.java
@@ -1,4 +1,32 @@
 //API : http://mabe02.github.io/lanterna/apidocs/2.1/
+/*
+import com.googlecode.lanterna;
+
+import com.googlecode.lanterna.bundle;
+
+import com.googlecode.lanterna.graphics;
+
+import com.googlecode.lanterna.gui2;
+
+import com.googlecode.lanterna.gui2.dialogs;
+
+import com.googlecode.lanterna.gui2.table;
+
+import com.googlecode.lanterna.input;
+
+import com.googlecode.lanterna.screen;
+
+import com.googlecode.lanterna.terminal;
+
+import com.googlecode.lanterna.terminal.ansi;
+
+import com.googlecode.lanterna.terminal.swing;
+
+import com.googlecode.lanterna.terminal.virtual;
+
+//import com.googlecode.lanterna.gui;
+//import com.googlecode.lanterna.terminal;
+//import com.googlecode.lanterna.screen;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.input.Key.Kind;
@@ -15,11 +43,26 @@ import com.googlecode.lanterna.gui.Window;
 import com.googlecode.lanterna.gui.dialog.TextInputDialog;
 import com.googlecode.lanterna.gui.GUIScreen;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.gui2;
+//import com.googlecode.lanterna.gui2;
+//import com.googlecode.lanterna.gui2.TextBox;
+
 //import com.googlecode.lanterna.gui.Panels;
-import com.googlecode.lanterna.gui2.AbstractComponent<T>;
-import com.googlecode.lanterna.gui2.AbstractInteractableComponent;<TextBox>;
+//import com.googlecode.lanterna.gui2.AbstractComponent;
+//import com.googlecode.lanterna.gui2.AbstractInteractableComponent;
 import com.googlecode.lanterna.gui2.TextBox;
+*/
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
 
 
 public class Search {
@@ -44,6 +87,7 @@ public class Search {
 
 
   public static void main(String[] args) {
+    boolean searching = true;
 
 
     Terminal terminal = TerminalFacade.createTerminal();
@@ -51,8 +95,23 @@ public class Search {
 
 		TerminalSize terminalSize = terminal.getTerminalSize();
 
-    Screen scr = new Screen(terminal);
-    GUIScreen gscr = new GUIScreen(scr);
+    putString(1,2,terminal,"Search for your book using its ISBN: ");
+
+
+    Key key = terminal.readInput();
+
+    if (key != null) {
+
+      if (key == Key.Kind.Enter) {
+        terminal.setCursorVisible(false);
+
+
+      }
+      searchTerm += key.getCharacter();
+
+
+    }
+
 
 
     //putString(1,2,terminal,"Search for your book using its ISBN: ");
@@ -66,8 +125,6 @@ public class Search {
     username.addComponent(new TextBox(null, 15));
     addComponent(username);
     */
-
-    TextBox test = new TextBox("Search for your ISBN");
 
 
   }
