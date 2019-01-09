6f8198aa322ca75a04251a2571d0735c1ab8993a
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-08 19:28:47 -0500

    Revert "in process of getting webpage to load" This reverts commit 01518984b875047fd7d2e8f379a37d2f31789bc6.

--- NYPLAccessor.java
+++ NYPLAccessor.java
@@ -1,27 +1,10 @@
-import java.net.*;
-import java.io.*;
-import javax.xml.parsers.*;
-import org.jsoup.*;
-import org.jsoup.nodes.*;
-
 public class NYPLAccessor implements CatalogAccessor{
   public Copy[] getAllCopies(Book bk){
-    Copy[] out = null;
-    try{
-      String[] ISBNs = bk.getISBNs();
-      String searchStr = genSearch(ISBNs);
-      String searchUrl = "https://browse.nypl.org/iii/encore/search/C__S"+searchStr+"__Orightresult__U?lang=eng&suite=def";
-      System.out.println(searchUrl);
-      URL searchPage = new URL(searchUrl);
-      Document html = Jsoup.connect(searchUrl).get();
-
-      return null;
-    }catch(MalformedURLException e){
-      e.printStackTrace();
-    }catch(IOException e){
-      e.printStackTrace();
-    }
-    return out;
+    String[] ISBNs = bk.getISBNs();
+    String searchStr = genSearch(ISBNs);
+    String searchUrl = "https://browse.nypl.org/iii/encore/search/C__S"+searchStr+"__Orightresult__U?lang=eng&suite=def";
+    System.out.println(searchUrl);
+    return null;
   }
   private String genSearch(String[] terms){
     return "%28"+String.join("%20%7C%20",terms)+"%29";
