62154b51600d79ef77fc6db2cfe508764cd935f0
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-08 20:01:37 -0500

    higher level method for extracting necessary HTML

--- NYPLAccessor.java
+++ NYPLAccessor.java
@@ -5,13 +5,17 @@ import java.util.*;
 public class NYPLAccessor implements CatalogAccessor{
   public Copy[] getAllCopies(Book bk){
     try{
+      System.out.println("start");
       String[] ISBNs = bk.getISBNs();
       String searchStr = genSearch(ISBNs);
       String searchUrl = "https://browse.nypl.org/iii/encore/search/C__S"+searchStr+"__Orightresult__U?lang=eng&suite=def";
       System.out.println(searchUrl);
       URL catSearch = new URL(searchUrl);
+      System.out.println("begin download");
       Scanner sca = new Scanner(catSearch.openStream());
+      System.out.println("begin extraction");
       ArrayList<String> htmlBlocks = getCopyHTML(sca);
+      System.out.println("complete");
       return null;
     }catch(IOException e){
       e.printStackTrace();
