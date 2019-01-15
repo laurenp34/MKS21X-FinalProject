a35303242ba2c00053ac6bb82ca3e2c5f1c9d20a
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-15 12:45:43 -0500

    display libraries with irregular length IDs (to be manually modified)

--- DownloadTester.java
+++ DownloadTester.java
@@ -3,7 +3,9 @@ public class DownloadTester{
   public static void main(String[] args)throws IOException{
     Branch[] branches = DownloadNYPLLocs.getBranches("NYPLLocations.json");
     for(int i=0;i<branches.length;i++){
-      System.out.println(branches[i].getID()+" "+branches[i].getName());
+      if(branches[i].getID().length() != 2){
+        System.out.println(i+" "+branches[i].getID()+" "+branches[i].getName());
+      }
     }
   }
 }
