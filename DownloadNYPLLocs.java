b72970266603cdc00035129d17ff9390f4670bca
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-15 09:05:29 -0500

    empty output file and imports for temporary download program

---
+++ DownloadNYPLLocs.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+
+public class DownloadNYPLLocs{
+  public static void main(String[] args){
+
+  }
+}
