55196269e8df87fed4f40baa9f8c52eaf22611a0
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-15 15:30:30 -0500

    removed unnecessary field of location name

--- Copy.java
+++ Copy.java
@@ -3,14 +3,12 @@ import org.w3c.dom.*;
 public class Copy{
   private Branch location;
   private String callnum;
-  private String locName;
   private String message;
   private String status;
   private boolean available;
   //private Date dueDate;
   public Copy(Branch loc,String callnum,String status,String message){
     this.location = loc;
-    locName = location.getName();
     this.callnum = callnum;
     this.status = status;
     this.message = message;
@@ -18,7 +16,7 @@ public class Copy{
     //ADD DATE CODE
   }
   public String toString(){
-    if(available) return "  Available Copy @"+locName;
-    else          return "Unavailable Copy @"+locName+" "+status;
+    if(available) return "  Available Copy @"+location.getName();
+    else          return "Unavailable Copy @"+location.getName()+" "+status;
   }
 }
