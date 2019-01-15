fd9810c6bedaef11e2cc9ad98bd120ee0617e347
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-15 10:29:53 -0500

    upper level structure

--- DownloadNYPLLocs.java
+++ DownloadNYPLLocs.java
@@ -5,7 +5,18 @@ import java.io.*;
 import java.util.*;
 
 public class DownloadNYPLLocs{
-  public static void main(String[] args){
+  public static void main(String[] args)throws IOException{
+    DownloadNYPLLocs runner = new DownloadNYPLLocs("https://refinery.nypl.org/api/nypl/locations/v1.0/locations");
+    FileWriter fw = new FileWriter("NYPLLocations.json");
+    BufferedWriter writer = new BufferedWriter(fw);
+    runner.writeData();
+    System.out.println("complete");
+  }
+  public DownloadNYPLLocs(String url){
 
   }
+  public void writeData(){
+    
+  }
+
 }
