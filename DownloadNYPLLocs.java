e17e1ce923237b78cbaedc623b0e30dac4de67fd
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-15 09:29:54 -0500

    found file with json of the necessary data, removed imports for xml parsing

--- DownloadNYPLLocs.java
+++ DownloadNYPLLocs.java
@@ -4,10 +4,6 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
-import org.xml.sax.*;
-
 public class DownloadNYPLLocs{
   public static void main(String[] args){
 
