0623553b76a2e76befaaa1b809461b3b53f47657
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-16 22:54:08 -0500

    Date.java with day and month instance variables

---
+++ Date.java
@@ -0,0 +1,17 @@
+public class Date {
+  public int day;
+  public int month;
+
+  public Date(int newDay, int newMonth) {
+    day = newDay;
+    month = newMonth;
+  }
+
+  public int getDay() {
+    return day;
+  }
+  public int getMonth() {
+    return month;
+  }
+
+}
