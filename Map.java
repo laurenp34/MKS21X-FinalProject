0544692bb25a2ab3bad31b154f1fd1028e384ece
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-01-16 22:55:17 -0500

    new file, imports and fields for Map

---
+++ Map.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import javax.xml.sax.*;
+
+
+public class Map{
+  private Branch[] branches;
+  private Document kml;
+}
