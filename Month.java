e6fc1270d14327f291724260f6523ad3c0a7f7e9
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-19 22:45:03 -0500

    new Month object to store array of dates. wrote constructor part to initialize array with correct length depending on month

---
+++ Month.java
@@ -0,0 +1,29 @@
+public class Month {
+  private String name;
+  private int num;
+  private int year;
+  private MyDate[] monthDays;
+  private String[] months = {"January","February","March","April","May","June","July","August","September"}
+,"October","Novemer","December"};
+  public Month(int numm,int yearr) {
+    num = numm;
+    year = yearr;
+    name = months[num-1];
+
+    if (num == 1 || num == 3 || num == 5 || num == 7 || num == 8 || num == 10 || num == 12) {
+      monthDays = new MyDate[31];
+    } else if (num == 2) {
+      if (yearr % 4 == 0) {
+        monthDays = new MyDate[29];
+      } else {
+        monthDays = new MyDate[28];
+      }
+    } else {
+      monthDays = new MyDate[30];
+    }
+
+
+
+  }
+
+}
