4df6d19dcc077235cffa6ea98d7ae62aff12fdc2
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-20 17:53:58 -0500

    added LocalDateTime object

--- LibraryCalendar.java
+++ LibraryCalendar.java
@@ -1,5 +1,16 @@
-public class LibraryCalendar {
+import java.time.LocalDateTime;
 
+public class LibraryCalendar {
+  MyDate today;
+
+  public LibraryCalendar()
+    LocalDateTime now = LocalDateTime.now();
+    int todayYear = now.getYear();
+    int todayMonth = now.getMonthValue();
+    int todayDay = now.getDayOfMonth();
+
+    today = new MyDate(todayMonth,todayDay,todayYear);
+  }
 
 
 }
