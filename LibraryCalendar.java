035ab8c645a6167b92c1ae8ac6cd10acd9877efa
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-20 18:14:49 -0500

    working on constructor. initialized today and Month[] cal

--- LibraryCalendar.java
+++ LibraryCalendar.java
@@ -2,14 +2,17 @@ import java.time.LocalDateTime;
 
 public class LibraryCalendar {
   MyDate today;
+  Month[] cal;
 
-  public LibraryCalendar()
+  public LibraryCalendar(Month firstMonth,Month lastMonth) {
     LocalDateTime now = LocalDateTime.now();
     int todayYear = now.getYear();
     int todayMonth = now.getMonthValue();
     int todayDay = now.getDayOfMonth();
 
     today = new MyDate(todayMonth,todayDay,todayYear);
+
+    cal = new Month[(lastMonth.getNum()-todayMonth)+1]; // assumes they're in the same year.
   }
 
 
