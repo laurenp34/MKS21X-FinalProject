a98197b62774e6b59de056a9665df6789964c7fd
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-21 18:09:39 -0500

    migrated the firstMonth lastMonth to create new libcal (from driver) into static method here

--- LibraryCalendar.java
+++ LibraryCalendar.java
@@ -30,5 +30,39 @@ public class LibraryCalendar {
     return cal;
   }
 
+  public static void newLibraryCalendar(Copy[] out) {
+
+    LocalDateTime now = LocalDateTime.now();
+    int nowYear = now.getYear();
+    int nowMonth = now.getMonthValue();
+    int nowDay = now.getDayOfMonth();
+
+    Month firstMonth = new Month(nowMonth,nowYear);
+    Month lastMonth = new Month(nowMonth,nowYear);
+
+    for (int i=0;i<out.length;i++) {
+      Copy c = out[i];
+      MyDate d = c.updateDueDMY();
+      int dDay = d.getDay();
+      int dMonth = d.getMonth();
+      int dYear = d.getYear();
+      //System.out.println(d);
+
+      /* no need to update first month b/c calendar won't span into the past.
+      if (dDay !=0 && ((dMonth < firstMonth.getNum()  && dYear <= firstMonth.getYear()) || dYear < firstMonth.getYear())) {
+        firstMonth = new Month(dMonth,dYear);
+      }*/
+      if (dDay !=0 && ((dMonth > lastMonth.getNum() && dYear >= lastMonth.getYear()) || dYear > lastMonth.getYear())) {
+        lastMonth = new Month(dMonth,dYear);
+      }
+    }
+
+    //System.out.println("first month: "+firstMonth.getNum()+" "+firstMonth.getYear());
+    //System.out.println("last month: "+lastMonth.getNum()+" "+lastMonth.getYear());
+
+    LibraryCalendar cal = new LibraryCalendar(firstMonth,lastMonth);
+
+  }
+
 
 }
