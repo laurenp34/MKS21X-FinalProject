de5434d5d5842dc91ee298f4b7affb0f34b432aa
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-01-21 19:08:51 -0500

    added addLater count variable

--- LibraryCalendar.java
+++ LibraryCalendar.java
@@ -30,7 +30,7 @@ public class LibraryCalendar {
     return cal;
   }
 
-  public static void newLibraryCalendar(Copy[] out) {
+  public static LibraryCalendar newLibraryCalendar(Copy[] out) {
 
     LocalDateTime now = LocalDateTime.now();
     int nowYear = now.getYear();
@@ -39,6 +39,8 @@ public class LibraryCalendar {
 
     Month firstMonth = new Month(nowMonth,nowYear);
     Month lastMonth = new Month(nowMonth,nowYear);
+
+    int dueLater = 0;
 
     for (int i=0;i<out.length;i++) {
       Copy c = out[i];
@@ -61,6 +63,7 @@ public class LibraryCalendar {
     //System.out.println("last month: "+lastMonth.getNum()+" "+lastMonth.getYear());
 
     LibraryCalendar cal = new LibraryCalendar(firstMonth,lastMonth);
+    return cal;
 
   }
 
